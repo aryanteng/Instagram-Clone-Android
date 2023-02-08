9bfed367e085fb213ba4b1098b684cac3416f1ac
aryanteng <aryanteng892@gmail.com>
2023-02-09 01:44:42 +0530

    retrieved data from firestore

--- app/src/main/java/com/example/instagramclone/PostsActivity.kt
+++ app/src/main/java/com/example/instagramclone/PostsActivity.kt
@@ -3,13 +3,30 @@ package com.example.instagramclone
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
+private const val TAG = "PostsActivity"
 class PostsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_posts)
+        val db = Firebase.firestore
+        val postsReference = db.collection("Posts")
+        postsReference.addSnapshotListener { snapshot, error ->
+            if(error != null || snapshot == null){
+                Log.i(TAG, "Error when querying posts", error)
+            }
+            if (snapshot != null) {
+                for (item in snapshot.documents){
+                    Log.i(TAG, "Document ${item.id}: ${item.data}")
+
+                }
+            }
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
